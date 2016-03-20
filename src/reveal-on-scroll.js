db0c471bfc788b0b8faea51155f895078baddbd9
Juned Chhipa <juned@ourphilosophy.co.uk>
2016-03-20 08:19:08 +0530

    animations changed to css

--- src/reveal-on-scroll.js
+++ src/reveal-on-scroll.js
@@ -43,7 +43,9 @@
                         if(revealed==false) {
                             setTimeout(function(){
                                 revealed = true;
-                                self.fadeIn(el, self.config.animationSpeed);
+                                var speed = self.config.animationSpeed;
+                                el.style.opacity = 1;
+                                el.style.transition = speed/1000 + 's all ease';
                             }, self.config.delay);
                         }
                     }
@@ -81,67 +83,6 @@
             var scrolledInEl = (elemTop >= 0) && (elemTop <= window.innerHeight);
             return scrolledInEl;
 
-        }
-
-        revealOnScroll.prototype.fadeIn = function(el, duration) {
-            // http://jsfiddle.net/LzX4s/
-            var to = 0;
-            var FX = {
-                easing: {
-                    linear: function(progress) {
-                        return progress;
-                    },
-                    quadratic: function(progress) {
-                        return Math.pow(progress, 2);
-                    },
-                    swing: function(progress) {
-                        return 0.5 - Math.cos(progress * Math.PI) / 2;
-                    },
-                    circ: function(progress) {
-                        return 1 - Math.sin(Math.acos(progress));
-                    },
-                    back: function(progress, x) {
-                        return Math.pow(progress, 2) * ((x + 1) * progress - x);
-                    },
-                    bounce: function(progress) {
-                        for (var a = 0, b = 1, result; 1; a += b, b /= 2) {
-                            if (progress >= (7 - 4 * a) / 11) {
-                                return -Math.pow((11 - 6 * a - 11 * progress) / 4, 2) + Math.pow(b, 2);
-                            }
-                        }
-                    },
-                    elastic: function(progress, x) {
-                        return Math.pow(2, 10 * (progress - 1)) * Math.cos(20 * Math.PI * x / 3 * progress);
-                    }
-                }
-            }
-            this.animate({
-                duration: duration,
-                delta: function(progress) {
-                    progress = this.progress;
-                    return FX.easing.swing(progress);
-                },
-                step: function(delta) {
-                    el.style.opacity = to + delta;
-                }
-            });
-        }
-
-        revealOnScroll.prototype.animate = function(options) {
-            var start = new Date;
-            var id = setInterval(function() {
-                var timePassed = new Date - start;
-                var progress = timePassed / options.duration;
-                if (progress > 1) {
-                    progress = 1;
-                }
-                options.progress = progress;
-                var delta = options.delta(progress);
-                options.step(delta);
-                if (progress == 1) {
-                    clearInterval(id);
-                }
-            }, options.delay || 10);
         }
 
         return revealOnScroll;
